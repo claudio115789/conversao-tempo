fc6719d3ab78fdda6616bf0cf94c952b3389592a
claudio115789 <claudio115789@gmail.com>
2020-12-30 23:06:40 -0300

    Create Conversao.cs

---
+++ Conversao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace conversao_tempo
+{
+    class Conversao
+    {
+        static void Main(string[] args)
+        {
+          
+      int entrada = Int32.Parse(Console.ReadLine());
+
+    int horasRestantes = entrada % 86400;
+    int minutosRestantes = horasRestantes % 3600;
+
+    int horas = entrada / 3600;
+    int minutos = (entrada%3600) / 60;
+    int segundos =minutosRestantes % 60;
+
+    Console.Write(horas + ":" + minutos + ":" + segundos);
+    }
+  }
+}
+
+       
